ba2bf0507ce14628bc9c167c747d188f995f48cb
imc786 <90568340+imc786@users.noreply.github.com>
2023-07-13 13:18:44 +0100

    Cycle dua

--- assets/js/time.js
+++ assets/js/time.js
@@ -292,7 +292,19 @@ async function timeCalc() {
   }
 }
 
+let images = ['assets/images/01.jpg', 'assets/images/02.jpg', 'assets/images/03.jpg', 'assets/images/04.jpg', 'assets/images/05.jpg', 'assets/images/06.jpg', 'assets/images/07.jpg', 'assets/images/08.jpg', 'assets/images/09.jpg', 'assets/images/10.jpg'];
+
+let index = 0;
+const imgElement = document.querySelector('#mainPhoto');
+
+function change() {
+   index > (images.length - 2) ? index = 0 : index++;
+   imgElement.src = images[index];
+}
+
 timeCalc(); // run immediately on page load, and then every 1 second
-var interval = setInterval(function() {
-  timeCalc();
-}, 1000);
+
+window.onload = function () {
+  setInterval(timeCalc, 1000); // timer every second
+  setInterval(change, 3000); // image swap every 20 seconds
+};

a7a9e8c5b86de5fa87fa6e3693431cdce715d5f8
imc786 <90568340+imc786@users.noreply.github.com>
2024-01-17 10:07:31 +0000

    Remove notice

--- assets/js/time.js
+++ assets/js/time.js
@@ -293,7 +293,7 @@ async function timeCalc() {
   }
 }
 
-let images = ['assets/images/01.jpg', 'assets/images/02.jpg', 'assets/images/03.jpg', 'assets/images/04.jpg', 'assets/images/05.jpg', 'assets/images/06.jpg', 'assets/images/07.jpg', 'assets/images/08.jpg', 'assets/images/09.jpg', 'assets/images/10.jpg', 'assets/images/11.jpg', 'assets/images/12.jpg'];
+let images = ['assets/images/01.jpg', 'assets/images/02.jpg', 'assets/images/03.jpg', 'assets/images/04.jpg', 'assets/images/05.jpg', 'assets/images/06.jpg', 'assets/images/07.jpg', 'assets/images/08.jpg', 'assets/images/09.jpg', 'assets/images/10.jpg'];
 
 let index = 0;
 const imgElement = document.querySelector('#mainPhoto');

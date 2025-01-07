55ca47194890d79b7718569b41c158a1eb385b0b
sislam2005 <shujaaul@icloud.com>
2025-01-07 13:42:45 +0000

    Update time.js

--- assets/js/time.js
+++ assets/js/time.js
@@ -215,8 +215,8 @@ let salahspace = `<h2>صلاة ${event[nextevent].ar} جماعة</h2>
   <h1 id="timediv" style="display: none;">
     ${event[nextevent].en} Jama'ah
   </h1>
-    <section id="sidebar" class="salah" style="display: none; background-color: #000;"></section>
-  <section id="prayerbar" style="display: none; background-color: #000;"></section>`;
+    <section id="sidebar" class="salah" style="display: none !important; background-color: #000!important;"></section>
+  <section id="prayerbar" style="display: none !important; background-color: #000 !important;"></section>`;
   
   let eventspace = `<h2><span id="nextar">${event[nextevent].en} time | وقت ${event[nextevent].ar}</span></h2>
   <span class="prayer">
